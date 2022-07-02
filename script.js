62276e0482a9db65a3e45e0a2c72c8a8e6ffd71d
Stephanie Gomez <stephaniemariagomez@gmail.com>
2022-07-02 12:47:34 -0700

    added moment(), now current date populates

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+//update day and date when application is opened
+var currentDayEl = moment().format("dddd, MMMM Do")
+$("#currentDay").text(currentDayEl);
+//console.log(currentDayEl)
+
+//ability to write in text boxes
+
+//button to safe user input into local storage
+
+//check indicator to alert user to when a new project has been saved
+
+//retrieval of project info from local storage
+
+//rows to change color to indicate if time slot is past/present/future
+    //if, else if, else argument?
